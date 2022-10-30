586864b62ff289b740eb220bbf5a59414219f0cd
yu nishisaki <ynishisaki@gmail.com>
2022-10-30 19:34:19 +0900

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,70 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Box, Center, Text, VStack } from "@chakra-ui/react";
 import { DecConvert } from "../components/DecConvert";
 import { HexConvert } from "../components/HexConvert";
 
 const Home: NextPage = () => {
-	return (
-		<div className={styles.container}>
-			<Head>
-				<meta charSet="utf-8" />
-				<title>Hex-Dec Converter</title>
-				<meta
-					name="description"
-					content="Conversion tool between hexadecimal and decimal. It supports unsigned integers and signed integers."
-				/>
-				<meta
-					name="google-site-verification"
-					content="QCMAUI4zWwgmC1eVBN1F1WtdAIgZlYkqhl2Mj9X4mKA"
-				/>
-				<meta name="author" content="monyo"></meta>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<main>
-				<Box layerStyle="body">
-					<Center layerStyle="title">Hex-Dec Converter</Center>
-					<VStack spacing={20} pb={10}>
-						<HexConvert />
-						<DecConvert />
-					</VStack>
-				</Box>
-			</main>
-			<footer>
-				<Box layerStyle="footer">
-					<Text>© 2022 monyo</Text>
-				</Box>
-			</footer>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <Head>
+                <meta charSet='utf-8' />
+                <title>Hex-Dec Converter</title>
+                <meta
+                    name='description'
+                    content='Conversion tool between hexadecimal and decimal. It supports unsigned integers and signed integers.'
+                />
+                <meta
+                    name='google-site-verification'
+                    content='QCMAUI4zWwgmC1eVBN1F1WtdAIgZlYkqhl2Mj9X4mKA'
+                />
+                <meta name='author' content='monyo'></meta>
+                {/* set favicon */}
+
+                <link
+                    rel='apple-touch-icon'
+                    sizes='180x180'
+                    href='/apple-touch-icon.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='32x32'
+                    href='/favicon-32x32.png'
+                />
+                <link
+                    rel='icon'
+                    type='image/png'
+                    sizes='16x16'
+                    href='/favicon-16x16.png'
+                />
+                <link rel='manifest' href='/site.webmanifest' />
+                <link
+                    rel='mask-icon'
+                    href='/safari-pinned-tab.svg'
+                    color='#5bbad5'
+                />
+                <meta name='msapplication-TileColor' content='#da532c' />
+                <meta name='theme-color' content='#ffffff' />
+            </Head>
+            <main>
+                <Box layerStyle='base'>
+                    <Center layerStyle='title'>Hex-Dec Converter</Center>
+                    <VStack spacing={20} pb={10}>
+                        <HexConvert />
+                        <DecConvert />
+                    </VStack>
+                </Box>
+            </main>
+            <footer>
+                <Box layerStyle='footer'>
+                    <Text>© 2022 monyo</Text>
+                </Box>
+            </footer>
+        </div>
+    );
 };
 
 export default Home;
--- theme.ts
+++ theme.ts
@@ -1,94 +1,74 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
-	components: {
-		Button: {
-			baseStyle: {},
-			defaultProps: {
-				colorScheme: "green",
-			},
-		},
-		Input: {
-			defaultProps: {
-				focusBorderColor: "green.400",
-			},
-		},
-		Select: {
-			defaultProps: {
-				focusBorderColor: "green.400",
-			},
-		},
-	},
+    components: {
+        Button: {
+            baseStyle: {},
+            defaultProps: {
+                colorScheme: "green",
+            },
+        },
+        Input: {
+            defaultProps: {
+                focusBorderColor: "green.400",
+            },
+        },
+        Select: {
+            defaultProps: {
+                focusBorderColor: "green.400",
+            },
+        },
+    },
 
-	//   textStyles: {
-	//     p: {
-	//       color: '#4d5156',
-	//       margin: '8px 0',
-	//       fontSize: '16px',
-	//       fontWeight: '200',
-	//       fontFamily: 'メイリオ, "Hiragino Sans", "ＭＳ Ｐゴシック", "Helvetica W01", sans-serif',
-	//       // lineHeight: '1.4',
-	//     },
-	//     h1: {
-	//       color: '#4d5156',
-	//       // margin: '16px 0',
-	//       fontSize: '16px',
-	//       fontWeight: 'bold',
-	//       fontFamily: 'メイリオ, "Hiragino Sans", "ＭＳ Ｐゴシック", "Helvetica W01", sans-serif;',
-	//     },
-	//     h2: {
-	//       color: '#4d5156',
-	//       // margin: '16px 0',
-	//       fontSize: '24px',
-	//       fontWeight: 'bold',
-	//       fontFamily: 'メイリオ, "Hiragino Sans", "ＭＳ Ｐゴシック", "Helvetica W01", sans-serif;',
-	//     },
-	//   },
+    textStyles: {},
 
-	layerStyles: {
-		body: {
-			maxW: "740px",
-			px: { base: "0", md: "10" },
-			mx: { base: "10vw", md: "auto" },
-			display: "block",
-		},
-		title: {
-			fontSize: "4xl",
-			py: "10",
-		},
-		header: {
-			px: "10px",
-			alignItems: "center",
-			display: "flex",
-			position: "fixed",
-			top: "0px",
-			height: "70px",
-			width: "100vw",
-			bgColor: "#f6f1eb",
-			zIndex: "banner", //1200
-		},
-		footer: {
-			borderTop: "1px solid #eaeaea",
-			flexDirection: "column",
-			display: "flex",
-			flex: "1",
-			w: "80vw",
-			mx: "auto",
-			p: "1rem 0",
-			justifyContent: "center",
-			alignItems: "center",
-			zIndex: "stickey", //1100
-		},
-		showBaseNumber: {
-			minW: "50px",
-			width: "35%", // 1/3
-			fontSize: "2xl",
-		},
-		showUnsignedOrSigned: {
-			minW: "150px",
-			width: "65%", // 2/3
-			fontSize: "lg",
-			ml: 2,
-		},
-	},
+    layerStyles: {
+        base: {
+            bg: "white",
+            maxW: "740px",
+            px: { base: "0", md: "10" },
+            mx: { base: "10vw", md: "auto" },
+            display: "block",
+            fontFamily:
+                '"Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif',
+        },
+        title: {
+            fontSize: "4xl",
+            py: "10",
+        },
+        header: {
+            px: "10px",
+            alignItems: "center",
+            display: "flex",
+            position: "fixed",
+            top: "0px",
+            height: "70px",
+            width: "100vw",
+            bgColor: "#f6f1eb",
+            zIndex: "banner", //1200
+        },
+        footer: {
+            borderTop: "1px solid #eaeaea",
+            flexDirection: "column",
+            display: "flex",
+            flex: "1",
+            w: "80vw",
+            mx: "auto",
+            p: "1rem 0",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: "stickey", //1100
+        },
+        showBaseNumber: {
+            minW: "50px",
+            width: "35%", // 1/3
+            fontSize: "2xl",
+        },
+        showUnsignedOrSigned: {
+            minW: "150px",
+            width: "65%", // 2/3
+            fontSize: "lg",
+            ml: 2,
+        },
+    },
 });
